77a029f1d12c6b3867ecbd3650c72a2d7eb58322
yinnping <ciaoing@livemail.tw>
2020-05-27 23:27:57 +0800

    Add tutorial 1

---
+++ hello-world/Send/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() 
+            {
+                HostName = "localhost"
+            };
+
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                
+            }
+        }
+    }
+}
