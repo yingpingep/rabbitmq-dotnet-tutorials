4f4e07ebdd9d409f2b98195fcb1335ef08521740
yinnping <ciaoing@livemail.tw>
2020-05-30 09:30:42 +0800

    [WIP] Start working on tutorial 2 part 2

---
+++ work-queues-manual-ack/NewTask/NewTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+            }
+        }
+
+    private static string GetMessage(string[] args)
+    {
+      return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    }
+  }
+}
