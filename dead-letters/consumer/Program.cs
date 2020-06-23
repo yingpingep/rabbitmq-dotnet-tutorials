989b6f0b9b734555006e01d44d1ba4b9a67b1b84
yinnping <ciaoing@livemail.tw>
2020-06-23 12:27:23 +0800

    Add dead-letters demo

---
+++ dead-letters/consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var exchangeName = "dead-letters";
+                var queueName = "grave";
+                channel.ExchangeDeclare(exchange: exchangeName,
+                                        type: ExchangeType.Direct,
+                                        durable: true,
+                                        autoDelete: false);
+
+                channel.QueueDeclare(queue: queueName,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false);
+
+                // 負責處理被送到 grave 的 dead-letters
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    Console.WriteLine($"Get message from {queueName}: '{message}'");
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: exchangeName,
+                                  routingKey: "grave");
+
+                channel.BasicConsume(queue: queueName,
+                                     consumer: consumer);
+                
+                Console.WriteLine("Enter to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ dead-letters/producer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "dead-letters",
+                                        type: ExchangeType.Direct,
+                                        durable: true,
+                                        autoDelete: false);
+                
+                Dictionary<string, Object> optionArgs = new Dictionary<string, object>();
+                                
+                // 指定 dead-letter-exchange 為 dead-letters
+                // 並重新指定 routing-key 為 grave                
+                optionArgs.Add("x-dead-letter-exchange", "dead-letters");
+                optionArgs.Add("x-dead-letter-routing-key", "grave");
+
+                // message 在 queue 只能存活 10 秒
+                optionArgs.Add("x-message-ttl", 10000);                
+
+                channel.QueueDeclare(queue: "first_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: optionArgs);
+
+
+                for (int i = 0; i < 10; i++)
+                {
+                    string message = $"This is a {i} message!";
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "first_queue",
+                                        basicProperties: null,
+                                        body: Encoding.UTF8.GetBytes(message));                    
+                }
+
+                Console.WriteLine("Message send.");
+            }
+        }
+    }
+}
